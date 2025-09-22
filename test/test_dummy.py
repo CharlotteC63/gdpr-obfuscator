ca336a978738a9521a82bff45b627a7af3d0c308
CharlotteC63 <ccampbell6066@gmail.com>
2025-09-22 13:51:05 +0200

    create dummy function in src file to check CI/CD is running correctly

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,7 +1,9 @@
 import pytest
+from src.dummy_file import dummy
 
 @pytest.mark.it(
         "Dummy test to check CI/CD"
     )
 def test_dummy():
-    assert True
+    result = dummy()
+    assert r
