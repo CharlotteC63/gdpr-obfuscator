9fbdf0fb7f74102d385cc6b5da4179c16ee010f6
CharlotteC63 <ccampbell6066@gmail.com>
2025-09-23 18:39:14 +0200

    create get_file_key util function using TDD

---
+++ tests/test_get_file_key.py
@@ -0,0 +1,48 @@
+import pytest
+from src.utils.get_file_key import get_file_key
+
+
+class TestGetFileKey:
+
+
+    @pytest.mark.it(
+        "When passed with a path in S3 URI format, returns correct key name"
+    )
+    def test_returns_key_name_from_path_in_S3_URI_format(self):
+        test_path = "s3://test-bucket/new_data/file.csv"
+        result = get_file_key(test_path)
+        assert result == "new_data/file.csv"
+
+    @pytest.mark.it(
+        "When passed with a path in HTTTP URL format, virtual-hosted style, returns correct file key"
+    )
+    def test_returns_key_name_from_path_in_HTTPS_virtual_hosted_format(self):
+        test_path = "https://test-bucket.s3.amazonaws.com/new_data/file.csv"
+        result = get_file_key(test_path)
+        assert result == "new_data/file.csv"
+
+    @pytest.mark.it(
+        "When passed with a path in HTTP URL format, path-style, returns correct file key"
+    )
+    def test_returns_key_name_from_path_in_HTTPS_path_style_format(self):
+        test_path = "https://s3.amazonaws.com/test-bucket/new_data/file.csv"
+        result = get_file_key(test_path)
+        assert result == "new_data/file.csv"
+
+    @pytest.mark.it(
+        "When passed with a path in ARN format, returns correct key name"
+    )
+    def test_returns_key_name_from_path_in_ARN_format(self):
+        test_path = "arn:aws:s3:::test-bucket/new_data/file.csv"
+        result = get_file_key(test_path)
+        assert result == "new_data/file.csv"
+
+    @pytest.mark.it(
+        "When passed with a path containing a key name that is over 1024 bytes in UTF-8, violating AWS guidelines, raises ValueError"
+    )
+    def test_raise_ValueError_when_key_name_exceeds_maximum_bytes(self):
+        test_key = ("a" * 1025)
+        test_path = f"s3://test-bucket/{test_key}"
+        with pytest.raises(ValueError) as err:
+            get_file_key(test_path)
+        assert str(err.value) == "S3 object key exceeds the maximum length of 1,024 bytes (UTF-8 encoded)"
