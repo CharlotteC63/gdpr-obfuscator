a91cb7eb37d06a4bf9396ef1b6587451be4c9ecd
CharlotteC63 <ccampbell6066@gmail.com>
2025-09-23 18:38:22 +0200

    use depencency injection (i.e. split into two functions) to reduce complexity of single function and improve testability

--- src/utils/get_bucket_name.py
+++ src/utils/get_bucket_name.py
@@ -1,33 +1,42 @@
 import re
 
-
 def get_bucket_name(path: str):
     if not isinstance(path, str):
         raise TypeError("Path to file must be a string")
+    
+    bucket_name = None
 
     try:
-        if isinstance(path, str):
-            if path.startswith("s3://"):
-                bucket_name = path.split("s3://", 1)[1].split("/", 1)[0]
+        if path.startswith("s3://"):
+            bucket_name = path.split("s3://", 1)[1].split("/", 1)[0]
 
-            elif path.startswith("https://"):
-                if path.startswith("https://s3.amazonaws.com"):
-                    bucket_name = path.split("https://s3.amazonaws.com/")[1].split(
-                        "/", 1
-                    )[0]
-                else:
-                    bucket_name = path.split("https://")[1].split(".", 1)[0]
+        elif path.startswith("https://"):
+            if path.startswith("https://s3.amazonaws.com"):
+                bucket_name = path.split("https://s3.amazonaws.com/")[1].split(
+                    "/", 1
+                )[0]
+            else:
+                bucket_name = path.split("https://")[1].split(".", 1)[0]
 
-            elif path.startswith("arn"):
-                bucket_name = path.split("arn:aws:s3:::")[1].split("/")[0]
+        elif path.startswith("arn:aws:s3:::"):
+            bucket_name = path.split("arn:aws:s3:::")[1].split("/")[0]
 
-            # checking that bucket_name fulfills aws naming rules (https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html):
-            if (
-                len(bucket_name) > 2
-                and len(bucket_name) < 64
-                and re.fullmatch(r"[a-z0-9.-]+", bucket_name) is not None
-            ):
-                return bucket_name
+        else:
+            raise ValueError("Unsupported path format")
+        
+    except (IndexError, ValueError):
+        raise ValueError("Invalid path format, could not extract bucket name")
 
-    except Exception:
-        raise ValueError("Invalid path provided")
+    if not check_validity_of_s3_bucket_name(bucket_name):
+        raise ValueError("Invalid bucket name according to AWS rules, see https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html")
+    
+    return bucket_name
+
+def check_validity_of_s3_bucket_name(name: str):
+    if not (3 <= len(name) <= 63):
+        return False
+    if not re.fullmatch(r'[a-z0-9][a-z0-9.-]*[a-z0-9]', name):
+        return False
+    if '..' in name or '.-' in name or '-.' in name:
+        return False
+    return True
