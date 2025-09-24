2a932489248e44b447a700745d7b0e3e00ced5ac
CharlotteC63 <ccampbell6066@gmail.com>
2025-09-24 10:59:34 +0200

    refactor to better handle errors, and add documentation

--- src/utils/get_file_key.py
+++ src/utils/get_file_key.py
@@ -2,33 +2,48 @@ from src.utils.get_bucket_name import get_bucket_name
 
 
 def get_file_key(path):
+    """
+    Gets the key of an S3 object from its file path.
+
+    Parameters
+    ----------
+    path : str
+        The path to the location of the file to be obfuscated. 
+        Must be in s3://..., arn:..., or https://... format.
+
+    Returns
+    ---------- 
+    str
+        A string of the file key.
+
+    Raises
+    ----------
+    ValueError
+        - When the bucket name in the path is invalid.
+        - When the path format is not recognised.
+        - When the path contains no key after the bucket name.
+        - When the key exceeds the maximum length of 1,024 bytes (UTF-8 encoded), 
+          according to AWS published rules.
+    
+    """
     bucket_name = get_bucket_name(path)
     key = None
+
+    if not bucket_name:
+        raise ValueError("Could not extract key name as bucket name is invalid")
+
     if path.startswith(("s3://", "arn")):
-        try:
-            key = path.split(f"{bucket_name}/", 1)[1]
-        except IndexError:
-            raise ValueError(
-                "Invalid S3 path format — key not found after bucket name."
-            )
+        key = path.split(f"{bucket_name}/", 1)[1]
 
-    elif path.startswith("https://"):
+    if path.startswith("https://"):
         if path.startswith("https://s3.amazonaws.com/"):
-            try:
-                key = path.split(f"{bucket_name}/", 1)[1]
-            except IndexError:
-                raise ValueError(
-                    "Invalid S3 URL format — key not found after bucket name."
-                )
+            key = path.split(f"{bucket_name}/", 1)[1]
         else:
-            try:
-                key = path.split(f"{bucket_name}.s3.amazonaws.com/", 1)[1]
-            except IndexError:
-                raise ValueError("Invalid S3 custom domain format — key not found.")
+            key = path.split(f"{bucket_name}.s3.amazonaws.com/", 1)[1]
 
-    else:
+    if key == "":
         raise ValueError(
-            "Path format not recognised. Must be s3://..., arn:..., or https://..."
+            "Key not found after bucket name"
         )
 
     if len(key.encode("utf-8")) > 1024:
@@ -37,3 +52,4 @@ def get_file_key(path):
         )
 
     return key
+        
