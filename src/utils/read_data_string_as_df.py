cf39bceac4e86f99a93f5db31ac655fe9582714a
CharlotteC63 <ccampbell6066@gmail.com>
2025-09-29 15:58:13 +0200

    update util functiton, read_data_string_as_df, so it can handle json file formats

--- src/utils/read_data_string_as_df.py
+++ src/utils/read_data_string_as_df.py
@@ -1,30 +1,55 @@
 import pandas as pd
 import io
+import json
 
 
-def read_data_string_as_df(csv_string):
-    if not csv_string:
+def read_data_string_as_df(data_string, file_type):
+    if not data_string:
         return None
-    possible_delimiters = [",", ";", "\t", "|"]
-    try:
-        csv_buffer = io.StringIO(csv_string)
-        if (
-            "," not in csv_string
-            and ";" not in csv_string
-            and "\t" not in csv_string
-            and "|" not in csv_string
-        ):
-            if "'" in csv_string:
-                return pd.read_csv(csv_buffer, quotechar="'")
-            return pd.read_csv(csv_buffer)
-        for file_delimiter in possible_delimiters:
-            if file_delimiter in csv_string and "'" in csv_string:
-                return pd.read_csv(csv_buffer, delimiter=file_delimiter, quotechar="'")
-            elif file_delimiter in csv_string:
-                return pd.read_csv(csv_buffer, delimiter=file_delimiter)
-            else:
-                continue
-    except Exception as err:
-        raise ValueError(
-            f"Failed to parse file due to invalid format or unknown delimiter. Details: {err}"
-        )
+    if file_type == "csv":
+        possible_delimiters = [",", ";", "\t", "|"]
+        try:
+            csv_buffer = io.StringIO(data_string)
+            if (
+                "," not in data_string
+                and ";" not in data_string
+                and "\t" not in data_string
+                and "|" not in data_string
+            ):
+                if "'" in data_string:
+                    return pd.read_csv(csv_buffer, quotechar="'")
+                return pd.read_csv(csv_buffer)
+            for file_delimiter in possible_delimiters:
+                if file_delimiter in data_string and "'" in data_string:
+                    return pd.read_csv(
+                        csv_buffer, delimiter=file_delimiter, quotechar="'"
+                    )
+                elif file_delimiter in data_string:
+                    return pd.read_csv(csv_buffer, delimiter=file_delimiter)
+                else:
+                    continue
+        except Exception as err:
+            raise ValueError(
+                f"Failed to parse csv file due to invalid format or unknown delimiter. Details: {err}"
+            )
+
+    if file_type == "json":
+        try:
+            data = json.loads(data_string)
+            data_as_list_of_dicts = None
+            if isinstance(data, dict):
+                dict_values = list(data.values())
+                if dict_values:
+                    first_value = dict_values[0]
+                    if isinstance(first_value, dict):
+                        data_as_list_of_dicts = [first_value]
+                    elif isinstance(first_value, list):
+                        data_as_list_of_dicts = first_value
+            elif isinstance(data, list):
+                data_as_list_of_dicts = data
+            return pd.DataFrame(data_as_list_of_dicts)
+        except ValueError:
+            raise ValueError("Unsupported json structure")
+
+    if file_type == "parquet":
+        pass
--- tests/test_read_data_string_as_df.py
+++ tests/test_read_data_string_as_df.py
@@ -8,7 +8,7 @@ class TestReadcsvasdf:
     @pytest.mark.it("When passed with an empty string, returns None")
     def test_returns_empty_list_when_file_contents_is_empty_list(self):
         csv_string = ""
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert result is None
 
     @pytest.mark.it(
@@ -16,7 +16,7 @@ class TestReadcsvasdf:
     )
     def test_returns_dataframe_for_non_empty_csv_string(self):
         csv_string = 'name\n"John Smith"'
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert isinstance(result, pd.DataFrame)
 
     @pytest.mark.it(
@@ -24,7 +24,7 @@ class TestReadcsvasdf:
     )
     def test_returns_dataframe_for_non_empty_csv_string_with_correct_contents(self):
         csv_string = 'name\n"John Smith"'
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert result.iloc[0]["name"] == "John Smith"
 
     @pytest.mark.it(
@@ -37,7 +37,7 @@ class TestReadcsvasdf:
         self,
     ):
         csv_string = "name\n'John Smith'"
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert result.iloc[0]["name"] == "John Smith"
 
     @pytest.mark.it(
@@ -47,7 +47,7 @@ class TestReadcsvasdf:
         self,
     ):
         csv_string = 'name;course\n"John Smith";"Software"'
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert result.iloc[0]["name"] == "John Smith"
         assert result.iloc[0]["course"] == "Software"
 
@@ -61,6 +61,89 @@ class TestReadcsvasdf:
         self,
     ):
         csv_string = "name;course\n'John Smith';'Software'"
-        result = read_data_string_as_df(csv_string)
+        result = read_data_string_as_df(csv_string, "csv")
         assert result.iloc[0]["name"] == "John Smith"
         assert result.iloc[0]["course"] == "Software"
+
+    @pytest.mark.it(
+        "When passed with a simple common format json string, returns pandas dataframe"
+    )
+    def test_returns_dataframe_when_passed_with_json_string(
+        self,
+    ):
+        json_string = '{"records":[{"name": "John Smith"}]}'
+        result = read_data_string_as_df(json_string, "json")
+        assert isinstance(result, pd.DataFrame)
+
+    @pytest.mark.it(
+        "When passed with a simple common format json string, returns pandas dataframe containing correct data"
+    )
+    def test_returns_dataframe_containing_correct_data_when_passed_with_simple_json_string_common_format(
+        self,
+    ):
+        json_string = '{"records":[{"name": "John Smith"}]}'
+        result = read_data_string_as_df(json_string, "json")
+        assert len(result.columns) == 1
+        assert len(result.index) == 1
+        assert result.iloc[0]["name"] == "John Smith"
+
+    @pytest.mark.it(
+        "When passed with a longer common format json string, returns pandas dataframe containing correct data"
+    )
+    def test_returns_dataframe_containing_correct_data_when_passed_with_long_json_string_common_format(
+        self,
+    ):
+        json_string = """{"records":[{"student_id": 1234,"name": "John Smith","course": "Software"},
+                                    {"student_id": 2222,"name": "Lily Allen","course": "DevOps"},
+                                    {"student_id": 3333,"name": "Paul Richmond","course": "Software"}]}"""
+        result = read_data_string_as_df(json_string, "json")
+        assert len(result.columns) == 3
+        assert len(result.index) == 3
+        assert result.iloc[0]["name"] == "John Smith"
+
+    @pytest.mark.it(
+        "When passed with a simple flat list format json string, returns pandas dataframe containing correct data"
+    )
+    def test_returns_dataframe_containing_correct_data_when_passed_with_simple_json_string_flat_list_format(
+        self,
+    ):
+        json_string = '[{"name": "John Smith"}]'
+        result = read_data_string_as_df(json_string, "json")
+        assert len(result.columns) == 1
+        assert len(result.index) == 1
+        assert result.iloc[0]["name"] == "John Smith"
+
+    @pytest.mark.it(
+        "When passed with a longer flat list format json string, returns pandas dataframe containing correct data"
+    )
+    def test_returns_dataframe_containing_correct_data_when_passed_with_long_json_string_flat_list_format(
+        self,
+    ):
+        json_string = """[{"student_id": 1234,"name": "John Smith","course": "Software"},
+                        {"student_id": 2222,"name": "Lily Allen","course": "DevOps"},
+                        {"student_id": 3333,"name": "Paul Richmond","course": "Software"}]"""
+        result = read_data_string_as_df(json_string, "json")
+        assert len(result.columns) == 3
+        assert len(result.index) == 3
+        assert result.iloc[0]["name"] == "John Smith"
+
+    @pytest.mark.it(
+        "When passed with a simple nested dictionary format json string, returns pandas dataframe containing correct data"
+    )
+    def test_returns_dataframe_containing_correct_data_when_passed_with_simple_json_string_nested_dict_format(
+        self,
+    ):
+        json_string = '{"records": {"name": "John Smith"}}'
+        result = read_data_string_as_df(json_string, "json")
+        assert len(result.columns) == 1
+        assert len(result.index) == 1
+        assert result.iloc[0]["name"] == "John Smith"
+
+    @pytest.mark.it("Raise ValueError when passed with unsupported json format")
+    def test_raises_ValueError_when_passed_with_unsupported_json_format(
+        self,
+    ):
+        json_string = "invalid_format"
+        with pytest.raises(ValueError) as err:
+            read_data_string_as_df(json_string, "json")
+        assert str(err.value) == "Unsupported json structure"
